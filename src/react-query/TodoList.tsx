87eb635bc2456c170607390a06f1aa5f6a7353ff
Maximilian <scientificresearch309@gmail.com>
2023-07-19 15:35:27 +0200

    removing the unnecessary data - the new Query Hook called useTodos.ts

--- src/react-query/TodoList.tsx
+++ src/react-query/TodoList.tsx
@@ -1,24 +1,10 @@
 import useTodos from './hooks/useTodos';
-// import React, { useEffect, useState } from 'react';
 
 const TodoList = () => {
 	const { data: todos, error, isLoading } = useTodos();
 
 	if (isLoading) return <p>Loading...</p>;
 	if (error) return <p>{error.message}</p>;
-
-	// We don't need the following data using react-query anymore:
-	//------------------------------------------------
-	// const [todos, setTodos] = useState<Todo[]>([]);
-	// const [error, setError] = useState('');
-
-	// useEffect(() => {
-	// 	axios
-	// 		.get('https://jsonplaceholder.typicode.com/todos')
-	// 		.then((res) => setTodos(res.data))
-	// 		.catch((error) => setError(error));
-	// }, []);
-	//------------------------------------------------
 
 	return (
 		<ul className="list-group">
