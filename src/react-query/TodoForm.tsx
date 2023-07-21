6b8dab30b8521329badea4629c05d830e1ff2849
Maximilian <scientificresearch309@gmail.com>
2023-07-21 14:00:26 +0200

    at this point, we can add the input field to the todo list.

--- src/react-query/TodoForm.tsx
+++ src/react-query/TodoForm.tsx
@@ -1,14 +1,28 @@
-import { useMutation } from '@tanstack/react-query';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { useRef } from 'react';
 import { Todo } from './hooks/useTodos';
 import axios from 'axios';
 
 const TodoForm = () => {
+	const queryClient = useQueryClient();
 	const addTodo = useMutation({
 		mutationFn: (todo: Todo) =>
 			axios
-				.post('https://jsonplaceholder.typicode.com/todos', todo)
+				.post<Todo>('https://jsonplaceholder.typicode.com/todos', todo)
 				.then((res) => res.data),
+		onSuccess: (saveTodo, newTodo) => {
+			// console.log(saveTodo);
+			//Approach:Invalidating the cache
+			// it doesn't work with API palceholder
+			// queryClient.invalidateQueries({
+			// 	queryKey: ['todos'],
+			// });
+			//Approach 2: updating the data in the cache directly:
+			queryClient.setQueryData<Todo[]>(['todos'], (todos) => [
+				saveTodo,
+				...(todos ? todos : []),
+			]);
+		},
 	});
 	const ref = useRef<HTMLInputElement>(null);
 
