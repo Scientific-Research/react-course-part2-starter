75ebc9e78d2eeb048e0a4b130b85e3f67354f2fa
Maximilian <scientificresearch309@gmail.com>
2023-07-26 15:01:26 +0200

    Organizing Code was done - we have now only three folders: auth,tasks,counter and no reducer, context and so on anymore!

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import './App.css';
-import AuthProvider from './state-management/AuthProvider';
+import AuthProvider from './state-management/auth/AuthProvider';
 import HomePage from './state-management/HomePage';
-import LoginStatus from './state-management/LoginStatus';
+import LoginStatus from './state-management/auth/LoginStatus';
 import NavBar from './state-management/NavBar';
 // import TaskProvider from './state-management/tasks/TaskProvider';
 import { TaskProvider } from './state-management/tasks';
--- src/state-management/auth/AuthProvider.tsx
+++ src/state-management/auth/AuthProvider.tsx
@@ -1,3 +1,6 @@
+import { ReactNode, useReducer } from 'react';
+import AuthContext from './authContext';
+
 interface LoginAction {
 	type: 'LOGIN';
 	username: string;
@@ -17,4 +20,18 @@ const authReducer = (state: string, action: AuthAction): string => {
 	return state;
 };
 
-export default authReducer;
+interface Props {
+	children: ReactNode;
+}
+
+const AuthProvider = ({ children }: Props) => {
+	const [user, dispatch] = useReducer(authReducer, '');
+
+	return (
+		<AuthContext.Provider value={{ user, dispatch }}>
+			{children}
+		</AuthContext.Provider>
+	);
+};
+
+export default AuthProvider;
