bdab66d547f8250fb18145247a49789f7f9d3e15
Maximilian <scientificresearch309@gmail.com>
2023-07-28 12:58:50 +0200

    Getting Data about the current Route - useParams(), searchParams,setSearchParams,.get,useLocation()

--- src/routing/UserDetailPage.tsx
+++ src/routing/UserDetailPage.tsx
@@ -1,5 +1,17 @@
+import { useLocation, useParams, useSearchParams } from 'react-router-dom';
+
 const UserDetailPage = () => {
-  return <p>User</p>;
+	const params = useParams();
+	// console.log(params);
+	const [searchParams, setSearchParams] = useSearchParams();
+	// console.log(searchParams.toString());
+	console.log(searchParams.get('name'));
+	console.log(searchParams.get('age'));
+
+	const location = useLocation();
+	console.log(location);
+
+	return <p>User</p>;
 };
 
 export default UserDetailPage;
