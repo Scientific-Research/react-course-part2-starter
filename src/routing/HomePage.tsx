ded806eb8d213f6b7c9daa5ebaa37cfbf475446c
Maximilian <scientificresearch309@gmail.com>
2023-07-28 12:14:57 +0200

    navigation - using Link instead of a href and using navigate to move between pages

--- src/routing/HomePage.tsx
+++ src/routing/HomePage.tsx
@@ -1,13 +1,16 @@
+import { Link } from 'react-router-dom';
+
 const HomePage = () => {
-  return (
-    <>
-      <p>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-        Incidunt, mollitia!
-      </p>
-      <a href="/users">Users</a>
-    </>
-  );
+	return (
+		<>
+			<p>
+				Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+				Incidunt, mollitia!
+			</p>
+			<Link to="/users">Users</Link>
+			{/* <a href="/users">Users</a> */}
+		</>
+	);
 };
 
 export default HomePage;
