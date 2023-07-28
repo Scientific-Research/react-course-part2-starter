23ce8abbd2fcfaf8cef2c6494eac6886e82ebf3f
Maximilian <scientificresearch309@gmail.com>
2023-07-28 16:39:58 +0200

    Handling Errors for home Page and invalid URLs =>invalid pages!

--- src/routing/ErrorPage.tsx
+++ src/routing/ErrorPage.tsx
@@ -1,10 +1,21 @@
+import { isRouteErrorResponse, useRouteError } from 'react-router-dom';
+
 const ErrorPage = () => {
-  return (
-    <>
-      <h1>Oops...</h1>
-      <p>Sorry, an unexpected error has occurred.</p>
-    </>
-  );
+	const error = useRouteError();
+	// console.log(error);
+	isRouteErrorResponse(error);
+
+	return (
+		<>
+			<h1>Oops...</h1>
+			{/* <p>Sorry, an unexpected error has occurred.</p> */}
+			<p>
+				{isRouteErrorResponse(error)
+					? 'Invalid page'
+					: 'Unexpected error'}
+			</p>
+		</>
+	);
 };
 
 export default ErrorPage;
