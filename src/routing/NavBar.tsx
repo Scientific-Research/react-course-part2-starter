febfd8388b4421bb9b84af57201a1bf0ca38aa1d
Maximilian <scientificresearch309@gmail.com>
2023-07-28 13:51:15 +0200

    Nested Routes - using Link in NavBar and children in routes.tsx and Outlet in Layout.tsx

--- src/routing/NavBar.tsx
+++ src/routing/NavBar.tsx
@@ -1,30 +1,38 @@
+import { Link } from 'react-router-dom';
+
 const NavBar = () => {
-  return (
-    <nav
-      className="navbar navbar-expand-lg"
-      style={{ background: '#f0f0f0', marginBottom: '1rem' }}
-    >
-      <div className="container-fluid">
-        <a className="navbar-brand" href="#">
-          My App
-        </a>
-        <div className="collapse navbar-collapse" id="navbarNav">
-          <ul className="navbar-nav">
-            <li className="nav-item">
-              <a className="nav-link active" href="#">
-                Home
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">
-                Users
-              </a>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </nav>
-  );
+	return (
+		<nav
+			className="navbar navbar-expand-lg"
+			style={{ background: '#f0f0f0', marginBottom: '1rem' }}
+		>
+			<div className="container-fluid">
+				<a className="navbar-brand" href="#">
+					My App
+				</a>
+				<div className="collapse navbar-collapse" id="navbarNav">
+					<ul className="navbar-nav">
+						<li className="nav-item">
+							<Link to="/" className="nav-link">
+								Home
+							</Link>
+							{/* <a className="nav-link active" href="#">
+								Home
+							</a> */}
+						</li>
+						<li className="nav-item">
+							<Link to="/users" className="nav-link">
+								Users
+							</Link>
+							{/* <a className="nav-link" href="#">
+								Users
+							</a> */}
+						</li>
+					</ul>
+				</div>
+			</div>
+		</nav>
+	);
 };
 
 export default NavBar;
